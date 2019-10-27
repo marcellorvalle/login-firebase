5ec3a9c0911290ff6440a6ea52847666627068ae
Marcello Valle <marcellorvalle@gmail.com>
2019-10-27 19:47:29 -0300

    Initial commit

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login"> 
+      <h1>loginComponent</h1>
+  </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component({})
+  export default class LoginComponent extends Vue {
+
+    mounted (){
+      console.log('hello from app');
+    }
+    
+  }
+
+</script>
+
+<style scoped lang="scss">
+  .login {
+
+  }
+</style>
