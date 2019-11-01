2894fc6c0ffa2b727677613615835d110ebdff68
Marcello Valle <marcellorvalle@gmail.com>
2019-10-31 22:10:37 -0300

    Update WebConfiguration.java
    Redirecionando páginas não  mapeadas para o Vue

--- backend/src/main/java/com/tutospringvue/backend/WebConfiguration.java
+++ backend/src/main/java/com/tutospringvue/backend/WebConfiguration.java
@@ -1,8 +1,14 @@
 package com.tutospringvue.backend;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -22,4 +28,16 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedMethods(methods);
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/spa")
+                .setViewName("forward:/index.html");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return factory -> factory.addErrorPages(
+                new ErrorPage(HttpStatus.NOT_FOUND,"/spa"));
+
+    }
 }
